5b077200d68ee17b728713677aca6c22961ae507
gabriielsilva5 <gabrielcsilva985@gmail.com>
2020-04-15 17:55:54 -0300

    finished challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  if( title == '' || url == '' || techs == '') {
+      response.status(400).json({erro: 'Campos vazios não são permitidos!'})
+  } else {
+      const repo = {id: uuid(), title, url, techs, likes: 0}
+      repositories.push(repo)
+
+      response.status(200).json(repo)
+  }
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repoAtuIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoAtuIndex < 0) {
+      response.status(400).json({ erro: "O id passado não existe!"})
+  } else {
+      const repo = { id: id, title, url, techs, likes: repositories[repoAtuIndex].likes }
+    
+      repositories[repoAtuIndex] = repo
+
+      response.status(200).json(repo)
+  }
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params
+  
+  const repoDelIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoDelIndex < 0) {
+      response.status(400).json({ erro: 'Não existem repositórios com este ID na base de dados!' })
+  } else {
+      repositories.splice(repoDelIndex, 1)
+      response.status(204).send()
+  }
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params
+
+  const repoLikeIndex = repositories.findIndex(repo => repo.id === id)
+
+  if(repoLikeIndex < 0){
+      response.status(400).json({erro: 'Não existem repositórios com este ID na base de dados!' })
+  } else {
+    repositories[repoLikeIndex].likes += 1
+    response.status(200).send()
+  }
+
+
 });
 
 module.exports = app;
